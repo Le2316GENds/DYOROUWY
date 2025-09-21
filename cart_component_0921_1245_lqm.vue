9d8fdb6401cf2c25a4441b28246ecc76b0f9e2c6
Le2316GENds <tuskslimzlaura@gmail.com>
2025-09-21 06:45:08 +0200

    Setup new 购物车功能实现 feature

---
+++ cart_component_0921_1245_lqm.vue
@@ -0,0 +1,41 @@
+// 代码生成时间: 2025-09-21 12:45:07
+import { reactive, onMounted } from 'vue';
+
+// 响应式数据
+const cart = reactive({
+  items: [],
+  count: 0,
+  addItem: function () {
+    this.items.push({ id: Date.now(), name: 'New Item', quantity: 1 });
+    this.count++;
+  },
+  removeItem: function (id) {
+    this.items = this.items.filter(item => item.id !== id);
+    this.count--;
+  },
+});
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted!');
+});
+
+// 导出响应式数据供模板使用
+export default {
+  data() {
+    return {
+      cart,
+    };
+  },
+  methods: {
+    addItem: cart.addItem,
+    removeItem: cart.removeItem,
+  },
+};
+</script>
+
+<style scoped>
+.cart {
+  /* styles here */
+}
+</style>
